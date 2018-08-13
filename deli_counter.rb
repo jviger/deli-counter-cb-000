c2031847710d9647442366446a687b651ca5d85f
Jason Viger <jason.viger@gmail.com>
2018-08-13 14:19:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,16 +4,14 @@ katz_deli = []
 
 def line(katz_deli)
   
-  if katz_deli.length == 0 
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else 
     message = "The line is currently:"
     line_order = ""
     katz_deli.each do |x|
-      line_order = line_order << "#{katz_deli.index(name) + 1}. #{x} "
+      line_order = line_order << "#{katz_deli.index(x) + 1}. #{x} "
     end
-    puts message << line_order
-  end
   
 end
 
