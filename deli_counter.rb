8c6cdb35a9c0d60e378ff39c735a78e64d7c269f
Jason Viger <jason.viger@gmail.com>
2018-08-13 13:56:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(deli)
+  
+  if deli.length == 0 
+    puts "The line is currently empty."
+  end
+  
+end
+
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}. You are number #{} in line."
+  
+end
